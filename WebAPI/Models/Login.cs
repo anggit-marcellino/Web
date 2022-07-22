6437fd23f895172aa9a4d8111103f253a3c4ee2e
anggit-marcellino <anggit.marcellino@iseller.id>
2022-07-22 09:11:41 +0700

    Add project files.

---
+++ WebAPI/Models/Login.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class Login
+    {
+        public string UserName
+        {
+            set;
+            get;
+        }
+        public string Password
+        {
+            set;
+            get;
+        }
+    }
+}
---
+++ WebAPI/Models/Response.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class Response
+    {
+        public string Status
+        {
+            set;
+            get;
+        }
+        public string Message
+        {
+            set;
+            get;
+        }
+    }
+}
