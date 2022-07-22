026caa9258335c5d81c9fa848dcedcde43848f19
anggit-marcellino <anggit.marcellino@iseller.id>
2022-07-23 01:12:19 +0700

    FixAPI

---
+++ WebAPI/Controllers/DosenController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [System.Web.Http.RoutePrefix("Api/Dosen")]
+    public class DosenController : ApiController
+    {
+
+        private WebAngularEntities1 objEntity = new WebAngularEntities1();
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route("AllDosens")]
+        public IQueryable<Dosen> GetDosen()
+        {
+            try
+            {
+                return objEntity.Dosens;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route("Prodi")]
+        public IQueryable<Prodi> GetProdi()
+        {
+            try
+            {
+                return objEntity.Prodis;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route("GetDosensById/{dosenId}")]
+        public IHttpActionResult GetDosenById(string dosenId)
+        {
+            Dosen objDos = new Dosen();
+            int Id = Convert.ToInt32(dosenId);
+            try
+            {
+                objDos = objEntity.Dosens.Find(Id);
+                if (objDos == null)
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return Ok(objDos);
+        }
+
+        [System.Web.Http.HttpPost]
+        [System.Web.Http.Route("InsertDosens")]
+        public IHttpActionResult PostDosen(Dosen data)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                objEntity.Dosens.Add(data);
+                objEntity.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return Ok(data);
+        }
+        [System.Web.Http.HttpPut]
+        [System.Web.Http.Route("UpdateDosens")]
+        public IHttpActionResult PutDosen(Dosen dosen)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                Dosen objDos = new Dosen();
+                objDos = objEntity.Dosens.Find(dosen.DosenId);
+                if (objDos != null)
+                {
+                    objDos.Nidn = dosen.Nidn;
+                    objDos.DosenName = dosen.DosenName;
+                    objDos.Prodi = dosen.Prodi;
+                    objDos.Address = dosen.Address;
+                    objDos.MailID = dosen.MailID;
+                    objDos.Age = dosen.Age;
+                    objDos.RegisteredDate = dosen.RegisteredDate;
+                }
+                int i = this.objEntity.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return Ok(dosen);
+        }
+
+        [System.Web.Http.HttpDelete]
+        [System.Web.Http.Route("DeleteDosens")]
+        public IHttpActionResult DeleteDosen(int id)
+        {  
+            Dosen dosen = objEntity.Dosens.Find(id);
+            if (dosen == null)
+            {
+                return NotFound();
+            }
+            objEntity.Dosens.Remove(dosen);
+            objEntity.SaveChanges();
+            return Ok(dosen);
+        }
+    }
+}
---
+++ WebAPI/Controllers/MahasiswaController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [System.Web.Http.RoutePrefix("Api/Mahasiswa")]
+    public class MahasiswaController : ApiController
+    {
+        private WebAngularEntities1 objEntity = new WebAngularEntities1();
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route("AllMahasiswas")]
+        public IQueryable<Mahasiswa> GetMahasiswa()
+        {
+            try
+            {
+                return objEntity.Mahasiswas;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route("Prodi")]
+        public IQueryable<Prodi> GetProdi()
+        {
+            try
+            {
+                return objEntity.Prodis;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route("GetMahasiswasById/{mahasiswaId}")]
+        public IHttpActionResult GetMahasiswaById(string mahasiswaId)
+        {
+            Mahasiswa objMah = new Mahasiswa();
+            int Id = Convert.ToInt32(mahasiswaId);
+            try
+            {
+                objMah = objEntity.Mahasiswas.Find(Id);
+                if (objMah == null)
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return Ok(objMah);
+        }
+
+        [System.Web.Http.HttpPost]
+        [System.Web.Http.Route("InsertMahasiswas")]
+        public IHttpActionResult PostMahasiswa(Mahasiswa data)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                objEntity.Mahasiswas.Add(data);
+                objEntity.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return Ok(data);
+        }
+        [System.Web.Http.HttpPut]
+        [System.Web.Http.Route("UpdateMahasiswas")]
+        public IHttpActionResult PutDosen(Mahasiswa mahasiswa)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                Mahasiswa objMah = new Mahasiswa();
+                objMah = objEntity.Mahasiswas.Find(mahasiswa.MahasiswaId);
+                if (objMah != null)
+                {
+                    objMah.Nim = mahasiswa.Nim;
+                    objMah.MahasiswaName = mahasiswa.MahasiswaName;
+                    objMah.Prodi = mahasiswa.Prodi;
+                    objMah.Address = mahasiswa.Address;
+                    objMah.MailID = mahasiswa.MailID;
+                    objMah.Age = mahasiswa.Age;
+                    objMah.RegisteredDate = mahasiswa.RegisteredDate;
+                }
+                int i = this.objEntity.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return Ok(mahasiswa);
+        }
+
+        [System.Web.Http.HttpDelete]
+        [System.Web.Http.Route("DeleteMahasiswas")]
+        public IHttpActionResult DeleteMahasiswa(int id)
+        {
+            Mahasiswa mahasiswa = objEntity.Mahasiswas.Find(id);
+            if (mahasiswa == null)
+            {
+                return NotFound();
+            }
+            objEntity.Mahasiswas.Remove(mahasiswa);
+            objEntity.SaveChanges();
+            return Ok(mahasiswa);
+        }
+    }
+}
